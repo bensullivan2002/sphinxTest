98e39e6c01fc4419bdfd759839dc8fdc34f68323
Ben Sullivan <bensullivan2002@gmail.com>
2024-02-02 11:04:10 +0000

    r

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ extensions = [
     'sphinx.ext.duration',
     'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary'
+    'sphinx.ext.autosummary',
     'sphinx.ext.nbsphinx'
 ]
 
