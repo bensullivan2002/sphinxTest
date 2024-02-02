21c31b3bd67d24ec8c5d8c6b5556af8398c68177
Ben Sullivan <bensullivan2002@gmail.com>
2024-02-02 11:40:43 +0000

    Changed theme to 'bizstyle'.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,5 +1,6 @@
 import pathlib
 import sys
+
 sys.path.insert(0, str(pathlib.Path(__file__).parents[2].resolve().as_posix()))
 
 # Configuration file for the Sphinx documentation builder.
@@ -34,3 +35,19 @@ exclude_patterns = []
 
 html_theme = 'sphinx-rtd-theme'
 html_static_path = ['_static']
+html_theme_options = {
+    'analytics_id': 'G-XXXXXXXXXX',  # Provided by Google in your dashboard
+    'analytics_anonymize_ip': False,
+    'logo_only': False,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'vcs_pageview_mode': '',
+    'style_nav_header_background': 'white',
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+}
