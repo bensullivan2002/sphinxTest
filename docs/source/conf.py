30169e1d4c2be7703512519a6aacfe0b05a68017
Ben Sullivan <bensullivan2002@gmail.com>
2024-02-02 12:31:13 +0000

    r

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,20 +35,4 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx-rtd-theme'
-# html_static_path = ['_static']
-html_theme_options = {
-    'analytics_id': 'G-XXXXXXXXXX',  # Provided by Google in your dashboard
-    'analytics_anonymize_ip': False,
-    'logo_only': False,
-    'display_version': True,
-    'prev_next_buttons_location': 'bottom',
-    'style_external_links': False,
-    'vcs_pageview_mode': '',
-    'style_nav_header_background': 'white',
-    # Toc options
-    'collapse_navigation': True,
-    'sticky_navigation': True,
-    'navigation_depth': 4,
-    'includehidden': True,
-    'titles_only': False
-}
+html_static_path = ['_static']
